651fda589a1f8c3599d3f2b9f6660600aa8b1071
medikent <kejohnson@mediciventures.com>
2018-10-10 19:27:10 -0600

    Add BaseSimulation and simple first test

--- src/gatling/scala/MyFirstTest.scala
+++ src/gatling/scala/MyFirstTest.scala
@@ -1,13 +1,21 @@
+import baseconfig.BaseSimulation
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 
-class MyFirstTest extends Simulation {
+class MyFirstTest extends BaseSimulation {
 
   // 1 Common HTTP Configuration
+  // get from BaseSimulation
 
   // 2 Scenario Definition
+  val scn = scenario("Get All Games Scenario")
+    .exec(http("Get All Games")
+      .get("videogames"))
 
   // 3 Load Scenario
+  setUp(
+    scn.inject(atOnceUsers(1))
+  ).protocols(httpConf)
 
 
 }
---
+++ src/gatling/scala/baseconfig/BaseSimulation.scala
@@ -0,0 +1,14 @@
+package baseconfig
+
+// Common imports for all simulations
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+class BaseSimulation extends Simulation {
+
+  val httpConf = http
+    .baseURL("http://localhost:8080/app/")
+    .header("Accept", "application/json")
+    .proxy(Proxy("localhost", 8888).httpsPort(8888))
+
+}
